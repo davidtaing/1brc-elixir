26963ad1a4edb9cf752441f5e9b8191157f5c702
David Taing <dave@teeang.net>
2024-08-16 23:02:32 +1000

    initial commit
    bootstrap new elixir project

---
+++ lib/brc_elixir.ex
@@ -0,0 +1,18 @@
+defmodule BrcElixir do
+  @moduledoc """
+  Documentation for `BrcElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BrcElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/brc_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule BrcElixirTest do
+  use ExUnit.Case
+  doctest BrcElixir
+
+  test "greets the world" do
+    assert BrcElixir.hello() == :world
+  end
+end
